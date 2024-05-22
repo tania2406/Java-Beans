2ac3bc32e6e1e224bf63a9befb91a42cbbeeae46
tania <taniaprasad4@gamil.com>
2024-05-22 14:11:35 +0530

    beans

---
+++ bean1.java
@@ -0,0 +1,23 @@
+package DSA;
+
+public class bean1 {
+int id;
+String name;
+public bean1()
+{
+	
+}
+public int getId() {
+	return id;
+} 
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+}
---
+++ beanex1.java
@@ -0,0 +1,13 @@
+package DSA;
+
+public class beanex1 {
+
+	public static void main(String args[])
+	{
+		bean1 b=new bean1();
+		b.setId(90);
+		b.setName("ranjeet singh");
+		System.out.println("name "+b.getName());
+		System.out.println("id "+b.getId());
+	}
+}
